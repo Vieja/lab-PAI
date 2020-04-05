5e7712fe0f5dca5aa9af0fcad19e1790459bddc6
Vieja <mwiechcz@gmail.com>
2020-04-05 21:00:35 +0200

    Add files via upload

---
+++ lab3/form_check.js
@@ -0,0 +1,91 @@
+function isWhiteSpaceOrEmpty(str) {
+ return /^[\s\t\r\n]*$/.test(str);
+}
+
+function IsEmailInvalid(str) {
+ let email = /^[a-zA-Z_0-9\.]+@[a-zA-Z_0-9\.]+\.[a-zA-Z][a-zA-Z]+$/;
+ return !email.test(str);
+}
+
+function checkStringAndFocus(obj, msg, func) {
+ let str = obj.value;
+ let errorFieldName = "e_" + obj.name.substr(2, obj.name.length);
+ if (func(str)) {
+ document.getElementById(errorFieldName).innerHTML = msg;
+ obj.focus();
+ return false;
+ }
+ else {
+ document.getElementById(errorFieldName).innerHTML = "";
+ return true;
+ }
+}
+
+function validate(formularz) {
+	if (!checkStringAndFocus(formularz.elements["f_imie"],"Podaj imię!",isWhiteSpaceOrEmpty)) {
+		return false;
+	}
+	if (!checkStringAndFocus(formularz.elements["f_nazwisko"],"Podaj nazwisko!",isWhiteSpaceOrEmpty)) {
+		return false;
+	}
+	if (!checkStringAndFocus(formularz.elements["f_ulica"],"Podaj ulicę!",isWhiteSpaceOrEmpty)) {
+		return false;
+	}
+	if (!checkStringAndFocus(formularz.elements["f_miasto"],"Podaj miasto!",isWhiteSpaceOrEmpty)) {
+		return false;
+	}
+	if (!checkStringAndFocus(formularz.elements["f_email"],"Podaj poprawny e-mail!",IsEmailInvalid)) {
+		return false;
+	}
+	return true;
+}
+
+function showElement(e) {
+ document.getElementById(e).style.visibility = 'visible';
+}
+function hideElement(e) {
+ document.getElementById(e).style.visibility = 'hidden';
+}
+
+function alterRows(i, e) {
+ if (e) {
+ if (i % 2 == 1) {
+ e.setAttribute("style", "background-color: Aqua;");
+ }
+ e = e.nextSibling;
+ while (e && e.nodeType != 1) {
+ e = e.nextSibling;
+ }
+ alterRows(++i, e);
+ }
+}
+
+function nextNode(e) {
+ while (e && e.nodeType != 1) {
+ e = e.nextSibling;
+ }
+ return e;
+}
+
+function prevNode(e) {
+ while (e && e.nodeType != 1) {
+ e = e.previousSibling;
+ }
+ return e;
+}
+
+function swapRows(b) {
+ let tab = prevNode(b.previousSibling);
+ let tBody = nextNode(tab.firstChild);
+ let lastNode = prevNode(tBody.lastChild);
+ tBody.removeChild(lastNode);
+ let firstNode = nextNode(tBody.firstChild);
+ tBody.insertBefore(lastNode, firstNode);
+}
+
+function cnt(form, msg, maxSize) {
+ if (form.value.length > maxSize)
+ form.value = form.value.substring(0, maxSize);
+ else
+ msg.innerHTML = maxSize - form.value.length;
+}
