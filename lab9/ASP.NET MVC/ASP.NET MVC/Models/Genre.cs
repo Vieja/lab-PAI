dcec0a17839345fef093e1da0306a397bced4f1f
Vieja <mwiechcz@gmail.com>
2020-05-25 13:51:28 +0200

    add lab9

---
+++ lab9/ASP.NET MVC/ASP.NET MVC/Models/Genre.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASP.NET_MVC.Models
+{
+    public class Genre
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Song> SongCollection;
+
+    }
+}
---
+++ lab9/ASP.NET MVC/ASP.NET MVC/Models/Song.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASP.NET_MVC.Models
+{
+    public class Song
+    {
+        public string Name { get; set; }
+        public string Artist { get; set; }
+        public int GenreId { get; set; }
+        public int Id { get; set; }
+    }
+}
